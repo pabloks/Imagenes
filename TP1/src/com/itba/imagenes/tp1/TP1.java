dd69de87522fc4591540ce54f1d76c37fddc7502
pabloks <pabloks@Apolo.(none)>
2010-08-25 20:37:09 -0700

    grey in params

--- TP1/src/com/itba/imagenes/tp1/TP1.java
+++ TP1/src/com/itba/imagenes/tp1/TP1.java
@@ -43,6 +43,8 @@ public class TP1 {
 				output = ImageUtils.getHistogram(inputImage1, params.color);
 			} else if (params.oper.equalsIgnoreCase("equalization")) {
 				output = ImageUtils.equalizate(inputImage1, params.color);
+			} else if (params.oper.equalsIgnoreCase("grey")) {
+				output = ImageUtils.blackAndWhite(inputImage1, params.color);
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
